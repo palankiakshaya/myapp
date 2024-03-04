c2c54b9ac74ed6adbadf77db6778e55fa89d5f33
akshaya <akshayapalanki@lyrostech.com>
2024-03-04 15:01:25 +0530

    yes

---
+++ src/App4.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Sqr from './Sqr';
+
+export default function App4() {
+   const sqrFunction=(num)=>{
+    return num*num;
+}
+return <div><Sqr n={4} f={sqrFunction}></Sqr></div>;
+}
