d9bdbd2ba31af47adc2164f9c44a86af27285549
akshaya <akshayapalanki@lyrostech.com>
2024-03-11 10:03:14 +0530

    h

---
+++ src/App17.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function App17() {
+  return (
+    <div>Hello World</div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App11 from './App11'
-import App16 from './App16'
+import App17 from './App17'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App16/>
+    <App17/>
   </React.StrictMode>
 );
