321ddcc6267befe964e632452f628a315bfda034
akshaya <akshayapalanki@lyrostech.com>
2024-03-18 15:17:11 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App11 from './App11'
-import App22 from './App22';
+// import App22 from './App22';
+import App24 from './App24';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App22/>
+    <App24/>
   </React.StrictMode>
 );
